a55bc10fda671a0f3a374953e9f5eb9e4ac99d4e
Vidar Kongsli <vidar@kongs.li>
2017-11-06 15:34:04 +0100

    Add function app. Add infrastructure provision script

---
+++ src/Kudu2SlackFuncApp/KuduDeployResult.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Kudu2SlackFuncApp
+{
+    public class KuduDeployResult
+    {
+        public string Id { get; set; }
+        public KuduStatus Status { get; set; }
+        public string StatusText { get; set; }
+        public string AuthorEmail { get; set; }
+        public string Author { get; set; }
+        public string Message { get; set; }
+        public string Deployer { get; set; }
+        public DateTimeOffset ReceivedTime { get; set; }
+        public DateTimeOffset StartTime { get; set; }
+        public DateTimeOffset EndTime { get; set; }
+        public DateTimeOffset LastSuccessEndTime { get; set; }
+        public bool Complete { get; set; }
+        public string SiteName { get; set; }
+        public string HostName { get; set; }
+        public bool IsSuccess => Status == KuduStatus.Success;
+
+        public enum KuduStatus
+        {
+            Failed,
+            Success
+        }
+    }
+}
---
+++ src/Kudu2SlackFuncApp/SlackMessage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Kudu2SlackFuncApp
+{
+    public class SlackMessage
+    {
+        [JsonProperty(PropertyName = "username")]
+        public string Username { get; set; }
+        [JsonProperty(PropertyName = "icon_emoji")]
+        public string IconEmoji { get; set; }
+        [JsonProperty(PropertyName = "text")]
+        public string Text { get; set; }
+        [JsonProperty(PropertyName = "channel")]
+        public string Channel { get; set; }
+        public static SlackMessage From(KuduDeployResult result)
+        {
+            var s = new SlackMessage
+            {
+                Username = (result.IsSuccess ? "Published: " : "Failed: ")
+                           + (result.SiteName ?? "unknown"),
+                IconEmoji = result.IsSuccess ? ":shipit:" : ":warning:",
+                Text = $@"Initiated by: {result.Author ?? "unknown"} at {result.EndTime:R}
+<https://{result.HostName}|{result.SiteName}> Id: {result.Id}{Environment.NewLine}```{result.Message}```",
+                Channel = "channel"
+            };
+            return s;
+        }
+        public SlackMessage ToChannel(string channel)
+        {
+            Channel = channel;
+            return this;
+        }
+    }
+}
