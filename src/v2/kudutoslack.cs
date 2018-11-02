82bfe58928b493621a31b148e5ddb2369b987004
Vidar Kongsli <vidar@kongs.li>
2018-11-02 13:50:00 +0100

    Add v2 version of func

---
+++ src/v2/kudutoslack.cs
@@ -0,0 +1,67 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using System.Net;
+
+namespace v2
+{
+    public static class kudutoslack
+    {
+        [FunctionName("kudutoslack")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            var webhook = req.Query[webhookParameter].ToString();
+            if (string.IsNullOrWhiteSpace(webhook)) {
+                return new BadRequestObjectResult($"Parameter '{webhookParameter}' is missing");
+            }
+            var channel = req.Query[channelParameter].ToString();
+            if (string.IsNullOrWhiteSpace(channel)) {
+                return new BadRequestObjectResult($"Parameter '{channelParameter}' is missing");
+            }
+
+            var incomingJsonData = await new StreamReader(req.Body).ReadToEndAsync();
+
+            log.LogInformation($"Payload: {incomingJsonData}");
+
+            if (string.IsNullOrWhiteSpace(incomingJsonData)) {
+                return new BadRequestObjectResult("No payload found in the request body");
+            }
+
+            var slackMessage =
+                JsonConvert.DeserializeObject<KuduDeployResult>(incomingJsonData)
+                    .Map(SlackMessage.From)
+                    .ToChannel(channel)
+                    .Map(JsonConvert.SerializeObject);
+
+            log.LogInformation($"Payload {incomingJsonData} transformed to {slackMessage}");
+            
+            var httpResponse = await _client.PostAsync(webhook,
+                slackMessage.Map(x => new StringContent(x, Encoding.UTF8, "application/json")));
+
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                return new OkResult();
+            }
+            var errorMessage = $@"Error processing, upstream webhook returned: {httpResponse.StatusCode} ({httpResponse.ReasonPhrase})";
+            log.LogError(errorMessage);
+            return new BadRequestObjectResult(errorMessage);
+        }
+
+        private const string webhookParameter = "webhook";
+        
+        private const string channelParameter = "channel";
+        
+        private static HttpClient _client = HttpClientFactory.Create();
+    }
+}
